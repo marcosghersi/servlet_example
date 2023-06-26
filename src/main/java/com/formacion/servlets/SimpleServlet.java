91e19c85b148fe1f013b44e324b684c46b1cc41a
Marcos <mamarting@minsait.com>
2023-06-26 09:16:46 +0200

    Subida primera del proyecto

---
+++ src/main/java/com/formacion/servlets/SimpleServlet.java
@@ -0,0 +1,33 @@
+package com.formacion.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -4751096228274971485L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>Hola desde el Pleistoceno!</h1>");
+		out.println("</body></html>");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
+	
+}
